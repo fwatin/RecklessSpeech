acabc8aa2d20679e7a762f5175afcbe945e39471
Felix <55741682+RogerHanain@users.noreply.github.com>
2022-07-23 16:58:44 +0200

    [Add-new-sequences] Add builder

---
+++ RecklessSpeech.Infrastructure.Sequences.Tests/DomainEvents/CaseOfSequenceEvents.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace RecklessSpeech.Infrastructure.Sequences.Tests.DomainEvents;
+
+public class CaseOfSequenceEvents
+{
+    [Fact]
+    public async Task ShouldSaveSequence()
+    {
+        
+    }
+
+    private static class Data
+    {
+        
+    }
+}
---
+++ RecklessSpeech.Shared.Tests/Sequences/SequenceBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using RecklessSpeech.Domain.Sequences;
+
+namespace RecklessSpeech.Shared.Tests.Sequences;
+
+public record SequenceBuilder(
+    string HtmlContent,
+    AudioFileNameWithExtension AudioFileNameWithExtension,
+    string Tags)
+{
+    public SequencesImportRequestedEvent BuildEvent() =>
+        new(this.HtmlContent, this.AudioFileNameWithExtension, this.Tags);
+
+    public static SequenceBuilder Create()
+    {
+        return new SequenceBuilder(Some.SomeHtml, AudioFileNameWithExtension.Hydrate(Some.SomeAudiofileNameWithExtension),
+            Some.SomeTags);
+    }
+
+    public string BuildUnformatedSequence()
+    {
+        return $"{Some.SomeHtml}	[sound:{Some.SomeAudiofileNameWithExtension}]	{Some.SomeTags}";
+    }
+}
