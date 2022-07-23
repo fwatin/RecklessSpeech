26898ed2be74762900d9b09b37250b1d3d423ae9
Felix <55741682+RogerHanain@users.noreply.github.com>
2022-07-23 16:33:07 +0200

    [Add-new-sequences] Make it work for two sequences

--- RecklessSpeech.Application.Write.Sequences.Tests/Sequences/Import/CaseOfImportSuccessful.cs
+++ RecklessSpeech.Application.Write.Sequences.Tests/Sequences/Import/CaseOfImportSuccessful.cs
@@ -20,18 +20,43 @@ public class CaseOfImportSuccessful
     public async Task Should_add_a_new_sequence()
     {
         //Arrange
-        string content =
-            "\"a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"";
-        ImportSequencesCommand command = new(content);
-        SequencesImportRequestedEvent expected = new("\"a lot of things in html\"",
-            AudioFileNameWithExtension.Hydrate("1658501397855.mp3"),
-            "\"word-naked lang-nl netflix Green pron \"");
+        ImportSequencesCommand command = new(Data.SomeContent);
 
         //Act
         IReadOnlyCollection<IDomainEvent> events = await this.sut.Handle(command);
 
         //Assert
-        events.Should().ContainEquivalentOf(expected);
+        events.Should().ContainEquivalentOf(Data.ExpectedEvent);
+    }
+
+    [Fact]
+    public async Task Should_add_two_sequences()
+    {
+        //Arrange
+        ImportSequencesCommand command = new(Data.SomeContentWithTwoSequences);
+
+        //Act
+        IReadOnlyCollection<IDomainEvent> events = await this.sut.Handle(command);
+
+        //Assert
+        events.Should().HaveCount(2);
+    }
+
+    private static class Data
+    {
+        public const string SomeHtml = "\"<style>a lot of things in html\"";
+        public const string SomeAudiofileName = "1658501397855.mp3";
+        public const string SomeTags = "\"word-naked lang-nl netflix Green pron \"";
+
+        public const string SomeContent =
+            "\"<style>a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"";
+
+        public const string SomeContentWithTwoSequences =
+            "\"<style>a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"" +
+            "\"<style>a lot of other things in html\"	[sound:123456.mp3]	\"some other tags \"";
+        
+
+        public static readonly SequencesImportRequestedEvent ExpectedEvent =
+            new(SomeHtml, AudioFileNameWithExtension.Hydrate(SomeAudiofileName), SomeTags);
     }
 }
-
