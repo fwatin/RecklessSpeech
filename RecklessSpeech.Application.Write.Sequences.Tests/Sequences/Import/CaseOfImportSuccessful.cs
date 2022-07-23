8ee2fce9feabdc2d9cc356ba383b8c4cd8592854
Felix <55741682+RogerHanain@users.noreply.github.com>
2022-07-23 16:58:44 +0200

    [Add-new-sequences] Add builder

--- RecklessSpeech.Application.Write.Sequences.Tests/Sequences/Import/CaseOfImportSuccessful.cs
+++ RecklessSpeech.Application.Write.Sequences.Tests/Sequences/Import/CaseOfImportSuccessful.cs
@@ -3,6 +3,8 @@ using FluentAssertions;
 using RecklessSpeech.Application.Write.Sequences.Commands;
 using RecklessSpeech.Domain.Sequences;
 using RecklessSpeech.Domain.Shared;
+using RecklessSpeech.Shared.Tests;
+using RecklessSpeech.Shared.Tests.Sequences;
 using Xunit;
 
 namespace RecklessSpeech.Application.Write.Sequences.Tests.Sequences.Import;
@@ -10,23 +12,41 @@ namespace RecklessSpeech.Application.Write.Sequences.Tests.Sequences.Import;
 public class CaseOfImportSuccessful
 {
     private readonly ImportSequencesCommandHandler sut;
+    private readonly SequenceBuilder sequenceBuilder;
 
     public CaseOfImportSuccessful()
     {
         this.sut = new ImportSequencesCommandHandler();
+        this.sequenceBuilder = SequenceBuilder.Create();
     }
 
     [Fact]
     public async Task Should_add_a_new_sequence()
     {
         //Arrange
-        ImportSequencesCommand command = new(Data.SomeContent);
+        ImportSequencesCommand command = new(Some.SomeSequenceContent);
 
         //Act
         IReadOnlyCollection<IDomainEvent> events = await this.sut.Handle(command);
 
         //Assert
-        events.Should().ContainEquivalentOf(Data.ExpectedEvent);
+        events.Should().HaveCount(1);
+        ((events.First() as SequencesImportRequestedEvent)!).HtmlContent.Should().NotBeNullOrEmpty();
+        ((events.First() as SequencesImportRequestedEvent)!).AudioFileNameWithExtension.Value.Should().NotBeNullOrEmpty();
+        ((events.First() as SequencesImportRequestedEvent)!).Tags.Should().NotBeNullOrEmpty();
+    }
+    
+    [Fact]
+    public async Task Should_add_a_known_sequence()
+    {
+        //Arrange
+        ImportSequencesCommand command = new(sequenceBuilder.BuildUnformatedSequence());
+
+        //Act
+        IReadOnlyCollection<IDomainEvent> events = await this.sut.Handle(command);
+
+        //Assert
+        events.Should().ContainEquivalentOf(sequenceBuilder.BuildEvent());
     }
 
     [Fact]
@@ -44,19 +64,8 @@ public class CaseOfImportSuccessful
 
     private static class Data
     {
-        public const string SomeHtml = "\"<style>a lot of things in html\"";
-        public const string SomeAudiofileName = "1658501397855.mp3";
-        public const string SomeTags = "\"word-naked lang-nl netflix Green pron \"";
-
-        public const string SomeContent =
-            "\"<style>a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"";
-
         public const string SomeContentWithTwoSequences =
             "\"<style>a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"" +
             "\"<style>a lot of other things in html\"	[sound:123456.mp3]	\"some other tags \"";
-        
-
-        public static readonly SequencesImportRequestedEvent ExpectedEvent =
-            new(SomeHtml, AudioFileNameWithExtension.Hydrate(SomeAudiofileName), SomeTags);
     }
 }
---
+++ RecklessSpeech.Shared.Tests/Some.cs
@@ -0,0 +1,10 @@
+﻿namespace RecklessSpeech.Shared.Tests;
+
+public static class Some
+{
+    public const string SomeHtml = "\"<style>a lot of things in html\"";
+    public const string SomeAudiofileNameWithExtension = "1658501397855.mp3";
+    public const string SomeTags = "\"word-naked lang-nl netflix Green pron \"";
+    public const string SomeSequenceContent =
+        "\"<style>a lot of things in html\"	[sound:1658501397855.mp3]	\"word-naked lang-nl netflix Green pron \"";
+}
