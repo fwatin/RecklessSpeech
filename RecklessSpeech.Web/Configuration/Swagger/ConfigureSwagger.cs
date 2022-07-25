5247327a43d703c8768b014ef004a203dfe15bf4
Felix <felix.watine.ext@agicap.com>
2022-07-25 18:49:24 +0200

    [Add-new-sequences] Configure swagger

---
+++ RecklessSpeech.Web/Configuration/Swagger/ConfigureSwagger.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace RecklessSpeech.Web.Configuration.Swagger
+{
+    public class ConfigureSwagger : IConfigureOptions<SwaggerGenOptions>, IConfigureOptions<SwaggerUIOptions>
+    {
+        private readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwagger(IApiVersionDescriptionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            options.CustomSchemaIds(x => x.FullName);
+
+            this.provider.ApiVersionDescriptions.ToList().ForEach(description =>
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Title = "RecklessSpeech Api " + description.GroupName.ToUpperInvariant(),
+                        Version = description.ApiVersion.ToString()
+                    }));
+        }
+
+        public void Configure(SwaggerUIOptions options)
+        {
+            this.provider.ApiVersionDescriptions.ToList().ForEach(description =>
+                options.SwaggerEndpoint(
+                    $"/swagger/{description.GroupName}/swagger.json",
+                    "RecklessSpeech Api " + description.GroupName.ToUpperInvariant()));
+        }
+    }
+}
