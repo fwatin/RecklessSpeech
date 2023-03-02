f149a2e7b2715feb33c9ee490bbe2e6621aeda91
Felix Watine <felix.watine@masteos.com>
2023-03-02 20:44:00 +0100

    nickel

--- UIAutomation/script_lln.js
+++ UIAutomation/script_lln.js
@@ -1,3 +1,5 @@
+var clickedWords = [];
+
 function ClickOnSubtitleWords() {
     console.log('start clicking...');
 
@@ -6,7 +8,7 @@ function ClickOnSubtitleWords() {
     for (let i = 0; i < totalCount; i++) {
         var element = document.querySelectorAll(".lln-word.lln-hover-tooltip.top")[i];
 
-        if (element.attributes["style"]?.value == "color: #ffbd80;") {
+        if (element.attributes["style"]?.value == "color: #ffbd80;" && !clickedWords.includes(element.attributes["data-word-key"]?.value)) {
 
             var event1 = new MouseEvent('contextmenu', {
                 bubbles: true,
@@ -14,25 +16,17 @@ function ClickOnSubtitleWords() {
                 button: 2 // bouton droit
               });
 
-              var event2 = new MouseEvent('contextmenu', {
-                bubbles: true,
-                cancelable: true,
-                button: 2 // bouton droit
-              });
               element.dispatchEvent(event1);
-              console.log('premier clic droit');
-               setTimeout(() => {
-               }, 100);
-               element.dispatchEvent(event2);
-               console.log('deuxieme clic droit');
+              clickedWords.push(element.attributes["data-word-key"]?.value);
+              console.log('ajout du mot : ' + element.attributes["data-word-key"]?.value);
         }
     }
 }
 
-// var i=0;
-// var intervalId = window.setInterval(function(){
-//   i=i+1;
+var i=0;
+var intervalId = window.setInterval(function(){
+  i=i+1;
   console.log('tour numéro: ' + i);
   console.log('Pour arrêter: clearInterval(' +intervalId +')');
   ClickOnSubtitleWords();
-//}, 2000);
+}, 1000);
