a1db5ddf52d61eee8d633a06f50f8ef2ec35c64a
Felix Watine <felix.watine@masteos.com>
2023-03-06 20:44:14 +0100

    fix

--- RecklessSpeech.Application.Write.Sequences.Tests/Sequences/SetDictionary/CaseOfSuccessful.cs
+++ RecklessSpeech.Application.Write.Sequences.Tests/Sequences/SetDictionary/CaseOfSuccessful.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using RecklessSpeech.Application.Write.Sequences.Commands;
+using RecklessSpeech.Application.Write.Sequences.Tests.Sequences.TestDoubles.Repositories;
 using RecklessSpeech.Domain.Sequences.Sequences;
 using RecklessSpeech.Domain.Shared;
 using RecklessSpeech.Infrastructure.Databases;
@@ -12,28 +13,26 @@ namespace RecklessSpeech.Application.Write.Sequences.Tests.Sequences.SetDictiona
 public class CaseOfSuccessful
 {
     private readonly AssignLanguageDictionaryCommandHandler sut;
-    private readonly InMemorySequencesDbContext dbContext;
     readonly Guid existingSequenceId = Guid.Parse("DAA0AAC9-8F31-46A2-AC73-5F6414B29F68");
+    private readonly AssignLanguageDictionaryCommand command;
+    private readonly SequenceBuilder sequenceBuilder;
 
     public CaseOfSuccessful()
     {
-        this.dbContext = new();
-        InMemorySequenceRepository sequenceRepository = new(this.dbContext);
-        this.sut = new AssignLanguageDictionaryCommandHandler(sequenceRepository);
-        SequenceBuilder sequenceBuilder = SequenceBuilder.Create(this.existingSequenceId);
-        this.dbContext.Sequences.Add(sequenceBuilder.BuildEntity());
+        InMemoryTestSequenceRepository sequenceRepository = new();
+        this.sut = new(sequenceRepository);
+        sequenceBuilder = SequenceBuilder.Create(this.existingSequenceId) with
+        {
+            LanguageDictionaryId = new(Guid.Parse("3E05B5FE-950B-4242-952D-0B926FF83D9B"))  
+        };
+        sequenceRepository.Feed(sequenceBuilder);
+        command = sequenceBuilder.BuildAssignDictionaryCommand();
+
     }
 
     [Fact]
     public async Task Should_set_dictionary_on_a_sequence()
     {
-        //Arrange
-        SequenceBuilder sequenceBuilder = SequenceBuilder.Create(this.existingSequenceId) with {
-        LanguageDictionaryId = new(Guid.Parse("3E05B5FE-950B-4242-952D-0B926FF83D9B"))  
-        };
-
-        AssignLanguageDictionaryCommand command = sequenceBuilder.BuildAssignDictionaryCommand();
-
         //Act
         IReadOnlyCollection<IDomainEvent> events = await this.sut.Handle(command, CancellationToken.None);
 
@@ -42,6 +41,6 @@ public class CaseOfSuccessful
             (AssignLanguageDictionaryInASequenceEvent) events.First(x => x is AssignLanguageDictionaryInASequenceEvent);
 
         dictionaryInASequenceEvent.SequenceId.Value.Should().Be(sequenceBuilder.SequenceId.Value);
-        dictionaryInASequenceEvent.LanguageDictionaryId!.Value.Should().Be(sequenceBuilder.LanguageDictionaryId.Value);
+        dictionaryInASequenceEvent.LanguageDictionaryId!.Value.Should().Be(sequenceBuilder.LanguageDictionaryId!.Value);
     }
 }
