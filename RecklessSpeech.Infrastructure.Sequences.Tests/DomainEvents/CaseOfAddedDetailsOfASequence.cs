6486308f15857ba608fe1ab16d007f8b52182f27
Felix Watine <felix.watine@masteos.com>
2023-03-07 15:06:57 +0100

    add test for ev

---
+++ RecklessSpeech.Infrastructure.Sequences.Tests/DomainEvents/CaseOfAddedDetailsOfASequence.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using RecklessSpeech.Domain.Sequences.Sequences;
+using RecklessSpeech.Infrastructure.Entities;
+using RecklessSpeech.Infrastructure.Orchestration.Dispatch;
+using RecklessSpeech.Shared.Tests.Sequences;
+using Xunit;
+
+namespace RecklessSpeech.Infrastructure.Sequences.Tests.DomainEvents;
+
+public class CaseOfAddedDetailsOfASequence : BaseInfrastructureTests
+{
+    [Fact]
+    public async Task ShouldAddDetails()
+    {
+        //Arrange
+        SequenceBuilder sequenceBuilder = SequenceBuilder.Create()with { TranslatedWord = null };
+        this.InMemorySequencesDbContext.Sequences.Add(sequenceBuilder.BuildEntity());
+        SetTranslatedWordEvent ev = new(new(sequenceBuilder.SequenceId.Value), new("bread"));
+        
+        //Act
+        await this.Sut.ApplyEvents(new List<DomainEventIdentifier>()
+            { new(Guid.Parse("6328FAC7-7AC9-4F3F-8652-9161FF345D4E"), ev) });
+
+        //Assert
+        SequenceEntity result = this.InMemorySequencesDbContext.Sequences.First();
+        result.TranslatedWord.Should().NotBeNull();
+    }
+}
--- RecklessSpeech.Infrastructure.Sequences.Tests/DomainEvents/CaseOfAddedSequence.cs
+++ RecklessSpeech.Infrastructure.Sequences.Tests/DomainEvents/CaseOfAddedSequence.cs
@@ -12,12 +12,13 @@ public class CaseOfAddedSequence : BaseInfrastructureTests
     public async Task ShouldSaveNewSequence()
     {
         //Arrange
-        SequenceBuilder sequenceBuilder = SequenceBuilder.Create(Guid.Parse("0CE0088F-256B-483A-9174-CAA40A558B05")) with{Explanation = null};
+        SequenceBuilder sequenceBuilder =
+            SequenceBuilder.Create(Guid.Parse("0CE0088F-256B-483A-9174-CAA40A558B05")) with { Explanation = null };
         SequenceEntity expectedEntity = sequenceBuilder.BuildEntity();
 
         //Act
         await this.Sut.ApplyEvents(new List<DomainEventIdentifier>()
-            {new(Guid.Parse("6328FAC7-7AC9-4F3F-8652-9161FF345D4E"), sequenceBuilder.BuildEvent())});
+            { new(Guid.Parse("6328FAC7-7AC9-4F3F-8652-9161FF345D4E"), sequenceBuilder.BuildEvent()) });
 
         //Assert
         SequenceEntity result = this.InMemorySequencesDbContext.Sequences.First();
