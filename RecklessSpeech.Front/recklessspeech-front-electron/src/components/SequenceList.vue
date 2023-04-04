2234455f51383557ac2d0ef5780240609998a23e
Felix Watine <felix.watine@masteos.com>
2023-04-04 16:30:45 +0200

    add list

--- RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
@@ -1,10 +1,22 @@
 <script>
+import axios from "axios";
 export default {
   setup() {},
   data() {
     return {
       filePickerDialog: false,
+      words: [],
     };
+  },
+  mounted() {
+    axios
+      .get("https://localhost:47973/api/v1/sequences")
+      .then((response) => {
+        this.words = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
   methods: {
     openFilePicker() {
@@ -15,9 +27,17 @@ export default {
 </script>
 <template>
   <div>
-    <b-button class="clickable" @click="openFilePicker"
-      >Import items.csv</b-button
-    >
+    <div>
+      <b-button class="clickable" @click="openFilePicker"
+        >Import items.csv</b-button
+      >
+    </div>
+
+    <div>
+      <b-table>
+        <b-table-simple>Cras justo odio</b-table-simple>
+      </b-table>
+    </div>
   </div>
 </template>
 
