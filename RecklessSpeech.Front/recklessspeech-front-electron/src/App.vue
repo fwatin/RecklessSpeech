0cd87fd2205ae6a1c4e3f9ffdb634d9a1a91de73
Felix Watine <felix.watine@masteos.com>
2023-04-04 15:48:10 +0200

    make modif

--- RecklessSpeech.Front/recklessspeech-front-electron/src/App.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron/src/App.vue
@@ -1,26 +1,71 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import fs from "fs";
+import pathModule from "path";
+import { computed, ref } from "vue";
+import { app } from "@electron/remote";
+import { file } from "@babel/types";
+
+const formatSize = (size) => {
+  var i = Math.floor(Math.log(size) / Math.log(1024));
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    " " +
+    ["B", "kB", "MB", "GB", "TB"][i]
+  );
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const path = ref(app.getAppPath());
+    const files = computed(() => {
+      const fileNames = fs.readdirSync(path.value);
+      return fileNames
+        .map((file) => {
+          const stats = fs.statSync(pathModule.join(path.value, file));
+          return {
+            name: file,
+            size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
+            directory: stats.isDirectory(),
+          };
+        })
+        .sort((a, b) => {
+          if (a.directory == b.directory) {
+            return a.name.localeCompare(b.name);
+          }
+          return a.directory ? -1 : 1;
+        });
+    });
+
+    const back = () => {
+      path.value = pathModule.dirname(path.value);
+    };
+
+    const open = (folder) => {
+      path.value = pathModule.join(path.value, folder);
+    };
+    const searchString = ref("");
+    const filteredFiles = computed(() => {
+      return searchString.value
+        ? files.value.filter((s) => s.name.startsWith(searchString.value))
+        : files.value;
+    });
+
+    return {
+      path,
+      open,
+      back,
+      files,
+      searchString,
+      filteredFiles,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="container mt-2">
+    <div class="form-group mt-4 mb-2">
+      <input class="form-control form-control-sm" placeholder="File search" />
+    </div>
+  </div>
+</template>
--- RecklessSpeech.Front/recklessspeech-front-electron/vue.config.js
+++ RecklessSpeech.Front/recklessspeech-front-electron/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+//commenté par moi car pas dans la vidéo
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true
+    }
+  }
+}
