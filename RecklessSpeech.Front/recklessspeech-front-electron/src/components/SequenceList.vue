538853156e69879ae95c33f7839295396f6fd664
Felix Watine <felix.watine@masteos.com>
2023-04-04 17:39:42 +0200

    improve style

--- RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
@@ -67,44 +67,69 @@ export default {
       }
 
       this.toast.info(
-        selectedWords.length + " séquences ont été envoyées vers Anki avec succès."
+        selectedWords.length +
+          " séquences ont été envoyées vers Anki avec succès."
       );
     },
     openFilePicker() {
       this.filePickerDialog = true;
     },
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+    },
     selectAll() {
-  if (this.checkedWords.some((isChecked) => isChecked)) {
-    // Au moins un élément est sélectionné, donc on les désélectionne tous
-    this.checkedWords = [];
-  } else {
-    // Aucun élément n'est sélectionné, donc on les sélectionne tous
-    this.checkedWords = this.words.map(() => true);
-  }
-}
-
-,
+      if (this.checkedWords.some((isChecked) => isChecked)) {
+        // Au moins un élément est sélectionné, donc on les désélectionne tous
+        this.checkedWords = [];
+      } else {
+        // Aucun élément n'est sélectionné, donc on les sélectionne tous
+        this.checkedWords = this.words.map(() => true);
+      }
+    },
   },
 };
 </script>
 <template>
   <div>
-    <div>
-      <button class="clickable" @click="openFilePicker">
-        Import items.csv
-      </button>
-      <button class="clickable" @click="enrichInEnglish()">
-        Enrichir en anglais
-      </button>
-      <button class="clickable" @click="enrichInDutch()">
-        Enrichir en néérlandais
-      </button>
-      <button class="clickable" @click="sendToAnki()">
-        Envoyer vers Anki
-      </button>
-      <button class="clickable" @click="selectAll()">
-        Selectionner tout
-      </button>
+    <div class="fieldset-container">
+      <fieldset style="border: 2px solid #000; padding: 10px">
+        <legend style="font-size: 20px">Sélectionner un fichier CSV</legend>
+        <p>
+          <input
+            type="file"
+            ref="fileInput"
+            accept=".csv"
+            @change="onFileSelected"
+          />
+          <button color="primary" @click="importCSV">Importer</button>
+        </p>
+      </fieldset>
+    </div>
+
+    <div class="fieldset-container">
+      <fieldset style="border: 2px solid #000; padding: 10px">
+        <legend style="font-size: 20px">Enrichir</legend>
+        <button class="clickable button-margin" @click="openFilePicker">
+          Import items.csv
+        </button>
+        <button class="clickable button-margin" @click="enrichInEnglish()">
+          Enrichir en anglais
+        </button>
+        <button class="clickable button-margin" @click="enrichInDutch()">
+          Enrichir en néérlandais
+        </button>
+        <button class="clickable button-margin" @click="selectAll()">
+          Selectionner tout
+        </button>
+      </fieldset>
+    </div>
+    <div class="fieldset-container">
+      <fieldset style="border: 2px solid #000; padding: 10px">
+        <legend style="font-size: 20px">Envoyer</legend>
+        <button class="clickable button-margin" @click="sendToAnki()">
+          Envoyer vers Anki
+        </button>
+      </fieldset>
     </div>
 
     <div>
@@ -112,7 +137,7 @@ export default {
         <tbody>
           <tr v-for="(file, index) in words" :key="file.name">
             <td>
-              <input type="checkbox" v-model="checkedWords[index]" />
+              <input type="checkbox" class="checkboxes" v-model="checkedWords[index]" />
               <span>{{ file.word }}</span>
             </td>
           </tr>
@@ -126,5 +151,14 @@ export default {
 <style scoped>
 .clickable {
   cursor: pointer;
+}
+.button-margin {
+  margin: 5px;
+}
+.fieldset-container{
+    margin: 5px;
+}
+.checkboxes{
+    margin: 5px;
 }
 </style>
