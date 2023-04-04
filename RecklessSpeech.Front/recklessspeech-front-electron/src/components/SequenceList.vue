942d90087358a0192e025cba21cbc318e164718f
Felix Watine <felix.watine@masteos.com>
2023-04-04 16:12:58 +0200

    add button and trigger on click works fine

--- RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron/src/components/SequenceList.vue
@@ -1,5 +1,29 @@
+<script>
+export default {
+  setup() {},
+  data() {
+    return {
+      filePickerDialog: false,
+    };
+  },
+  methods: {
+    openFilePicker() {
+      this.filePickerDialog = true;
+    },
+  },
+};
+</script>
 <template>
   <div>
-    <b-button>Import items.csv</b-button>
+    <b-button class="clickable" @click="openFilePicker"
+      >Import items.csv</b-button
+    >
   </div>
 </template>
+
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+</style>
