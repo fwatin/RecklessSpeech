cbf22710208951d043759b773b7aa7eb7be5f5db
Felix Watine <felix.watine@masteos.com>
2023-04-04 18:08:21 +0200

    clean actions npm run

--- RecklessSpeech.Front/recklessspeech-front-electron/vue.config.js
+++ RecklessSpeech.Front/recklessspeech-front-electron/vue.config.js
@@ -1,9 +1,3 @@
-//commenté par moi car pas dans la vidéo
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
 module.exports = {
   pluginOptions: {
     electronBuilder: {
