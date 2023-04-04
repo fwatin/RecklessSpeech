bcdcd7e66b1137c6a6ff76faa8e8be61f520df5e
Felix Watine <felix.watine@masteos.com>
2023-04-04 17:41:45 +0200

    clean remove example component

--- RecklessSpeech.Front/recklessspeech-front-electron/src/App.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron/src/App.vue
@@ -1,71 +1,9 @@
 <script>
-import fs from "fs";
-import pathModule from "path";
-
-import { app } from "@electron/remote";
-import { computed, ref } from "vue";
-
-import FilesViewer from "./components/FilesViewer";
 import SequenceList from "./components/SequenceList";
-
-const formatSize = (size) => {
-  var i = Math.floor(Math.log(size) / Math.log(1024));
-  return (
-    (size / Math.pow(1024, i)).toFixed(2) * 1 +
-    " " +
-    ["B", "kB", "MB", "GB", "TB"][i]
-  );
-};
 
 export default {
   name: "App",
-  components: { SequenceList, FilesViewer },
-  setup() {
-    const path = ref(app.getAppPath());
-    const files = computed(() => {
-      const fileNames = fs.readdirSync(path.value);
-      return fileNames
-        .map((file) => {
-          const stats = fs.statSync(pathModule.join(path.value, file));
-          return {
-            name: file,
-            size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
-            directory: stats.isDirectory(),
-          };
-        })
-        .sort((a, b) => {
-          if (a.directory === b.directory) {
-            return a.name.localeCompare(b.name);
-          }
-          return a.directory ? -1 : 1;
-        });
-    });
-
-    const back = () => {
-      path.value = pathModule.dirname(path.value);
-    };
-    const open = (folder) => {
-      path.value = pathModule.join(path.value, folder);
-    };
-
-    const searchString = ref("");
-    const filteredFiles = computed(() => {
-      return searchString.value
-        ? files.value.filter((s) => s.name.startsWith(searchString.value))
-        : files.value;
-    });
-
-    return {
-      path,
-
-      open,
-      back,
-
-      files,
-      searchString,
-      filteredFiles,
-    };
-  },
+  components: { SequenceList },
 };
 </script>
 
@@ -73,12 +11,5 @@ export default {
   <div class="container mt-2">
     <SequenceList>
     </SequenceList>
-
-    <FilesViewer
-      :files="filteredFiles"
-      :nested="nested"
-      @back="back"
-      @folderclick="open($event.name)"
-    />
   </div>
 </template>
