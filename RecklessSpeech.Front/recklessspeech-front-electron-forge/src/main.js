07aafe305d4425da5c12b1190cc849c985f07fe3
Felix Watine <felix.watine@masteos.com>
2023-04-05 19:02:05 +0200

    try solve with preload.js

--- RecklessSpeech.Front/recklessspeech-front-electron-forge/src/main.js
+++ RecklessSpeech.Front/recklessspeech-front-electron-forge/src/main.js
@@ -12,7 +12,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+      preload: path.join(__dirname, 'preload.js'), // Add this line
     },
   });
 
--- RecklessSpeech.Front/recklessspeech-front-electron-forge/src/preload.js
+++ RecklessSpeech.Front/recklessspeech-front-electron-forge/src/preload.js
@@ -1,2 +1,18 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { webFrame } = require('electron');
+
+webFrame.registerURLSchemeAsPrivileged('http', {
+  bypassCSP: true,
+  secure: false,
+  supportFetchAPI: true,
+  allowServiceWorkers: false,
+  corsEnabled: true,
+});
+
+webFrame.executeJavaScript(`
+  document.addEventListener('DOMContentLoaded', () => {
+    const csp = document.createElement('meta');
+    csp.setAttribute('http-equiv', 'Content-Security-Policy');
+    csp.setAttribute('content', "default-src 'self' 'unsafe-inline' data:; connect-src 'self' https://localhost:47973; script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+    document.head.appendChild(csp);
+  });
+`);
