fe5a636519ee869863607474fd89ee84fee48a0b
Felix Watine <felix.watine@masteos.com>
2023-04-05 18:05:26 +0200

    add Hello Vue !

--- RecklessSpeech.Front/recklessspeech-front-electron-forge/src/renderer.js
+++ RecklessSpeech.Front/recklessspeech-front-electron-forge/src/renderer.js
@@ -1,31 +1,13 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+// Since we declared the script as type=module in the HTML file,
+// we can use ES Modules (adapted from the Vue 2 Introduction
+// https://vuejs.org/v2/guide/#Declarative-Rendering
 
-import './index.css';
+// Alternatively, omit the .min from the path for Vue debugging purposes.
+import Vue from '../node_modules/vue/dist/vue.esm.browser.min.js';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
