fe3aa8ef3dd85d882291fb209b196f94037ef3b9
Felix Watine <felix.watine@masteos.com>
2023-04-05 17:26:31 +0200

    add vue

---
+++ RecklessSpeech.Front/recklessspeech-front-electron-forge/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import SequenceList from "./components/SequenceList";
+
+export default {
+  name: "App",
+  components: { SequenceList },
+};
+</script>
+
+<template>
+  <div class="container mt-2">
+    <SequenceList>
+    </SequenceList>
+  </div>
+</template>
