fc92514f23cf3e560315551b1fa3713b837eb2e0
Felix Watine <felix.watine@masteos.com>
2023-04-20 15:15:57 +0200

    fix

--- RecklessSpeech.Infrastructure.Sequences/Gateways/ChatGpt/ChatGptGateway.cs
+++ RecklessSpeech.Infrastructure.Sequences/Gateways/ChatGpt/ChatGptGateway.cs
@@ -1,6 +1,7 @@
 ﻿using RecklessSpeech.Application.Write.Sequences.Ports.TranslatorGateways.Dutch;
 using RecklessSpeech.Domain.Sequences.Explanations;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
 {
@@ -18,7 +19,7 @@ namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
             ChatGptRequest request = new("gpt-3.5-turbo",
                 new()
                 {
-                    new("user", $"Peux tu expliquer le sens du mot {language.GetLanguageInFrench()} {word} dans la phrase {sentence}")
+                    new("user", $"Peux-tu expliquer le sens du mot {language.GetLanguageInFrench()} {word} dans la phrase {sentence}")
                 });
 
             HttpRequestMessage requestMessage = new(HttpMethod.Post, "chat/completions")
@@ -27,7 +28,11 @@ namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
             };
             HttpResponseMessage httpResponseMessage = await this.client.SendAsync(requestMessage);
 
-            ChatGptResponse? response = await httpResponseMessage.Content.ReadFromJsonAsync<ChatGptResponse>();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            ChatGptResponse? response = await httpResponseMessage.Content.ReadFromJsonAsync<ChatGptResponse>(options);
 
             if (response is null) throw new("unexpected ChatGpt response is null");
             
@@ -37,8 +42,8 @@ namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
             string content = template
                 .Replace("{{word}}", word)
                 .Replace("{{sentence}}", sentence)
-                .Replace("{{explanation}}", response.choices.First().message.content);
-
+                .Replace("{{language}}", language.GetLanguageInFrench())
+                .Replace("{{explanation}}", response.choices.First().Message.Content);
 
             return Explanation.Create(Guid.NewGuid(),content, word, "ChatGpt");
         }
--- RecklessSpeech.Infrastructure.Sequences/Gateways/ChatGpt/ChatGptResponse.cs
+++ RecklessSpeech.Infrastructure.Sequences/Gateways/ChatGpt/ChatGptResponse.cs
@@ -1,39 +1,32 @@
-﻿// ReSharper disable once InconsistentNaming
-// ReSharper disable UnusedMember.Global
-// ReSharper disable InconsistentNaming
-// ReSharper disable ClassNeverInstantiated.Global
-namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
+﻿namespace RecklessSpeech.Infrastructure.Sequences.Gateways.ChatGpt
 {
     public class ChatGptResponse
     {
-#pragma warning disable CS8618
-        public string id { get; set; }
-        public string _object { get; set; }
+        public string? Id { get; set; }
+        public string? Object { get; set; }
         public int created { get; set; }
-        public string model { get; set; }
-        public Usage usage { get; set; }
-        public Choice[] choices { get; set; }
+        public string? model { get; set; }
+        public Usage? usage { get; set; }
+        public Choice[]? choices { get; set; }
     }
 
     public class Usage
     {
-        public int prompt_tokens { get; set; }
-        public int completion_tokens { get; set; }
-        public int total_tokens { get; set; }
+        public int PromptTokens { get; set; }
+        public int CompletionTokens { get; set; }
+        public int TotalTokens { get; set; }
     }
 
     public class Choice
     {
-        public Message message { get; set; }
-        public string finish_reason { get; set; }
+        public Message? Message { get; set; }
+        public string? FinishReason { get; set; }
         public int index { get; set; }
     }
 
     public class Message
     {
-        public string role { get; set; }
-        public string content { get; set; }
+        public string? Role { get; set; }
+        public string? Content { get; set; }
     }
-#pragma warning restore CS8618
-
 }
