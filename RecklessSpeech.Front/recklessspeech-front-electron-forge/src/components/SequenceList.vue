95dcd0f14e7d79508ae5e6bb76a881457befb911
Felix <55741682+fwatin@users.noreply.github.com>
2023-05-15 14:31:47 +0200

    fix: import json had error + add multiple selection in sequence list

--- RecklessSpeech.Front/recklessspeech-front-electron-forge/src/components/SequenceList.vue
+++ RecklessSpeech.Front/recklessspeech-front-electron-forge/src/components/SequenceList.vue
@@ -6,50 +6,77 @@ export default {
     return {
       filePickerDialog: false,
       jsonPickerDialog: false,
-      words: [],
-      checkedWords: [],
+      sequences: [],
+      checkedSequences: [],
       selectedFile: null,
       selectedJson: null,
       enrichProgression: 0,
       isEnriching: false,
       isSendingToAnki: false,
       sendToAnkiProgression: 0,
+      lastSelectedSequenceIndex: null,
     };
   },
   methods: {
-    async enrichInDutch() {
-      const selectedWords = this.words.filter((word, index) => {
-        return this.checkedWords[index];
-      });
-      this.enrichProgression = 0;
-      this.isEnriching = true;
-      let enrichCount = 0;
-      let total = selectedWords.length;
-      for (const sequence of selectedWords) {
-        let id = sequence.id;
-        await axios.post(
-          `https://localhost:${backendPort}/api/v1/sequences/Dictionary/dutch?id=${id}`
-        );
-        enrichCount++;
-        this.enrichProgression = Math.round((enrichCount * 100) / total);
+    selectSequence(index, isShiftPressed) {
+      if (isShiftPressed && this.lastSelectedSequenceIndex !== null) {
+        const start = Math.min(this.lastSelectedSequenceIndex, index);
+        const end = Math.max(this.lastSelectedSequenceIndex, index);
+        this.checkedSequences = [];
+        for (let i = start; i <= end; i++) {
+          this.checkedSequences.push(i);
+        }
+      } else {
+        this.lastSelectedSequenceIndex = index;
+        const isChecked = this.checkedSequences.includes(index);
+        if (isChecked) {
+          this.checkedSequences = this.checkedSequences.filter(
+            (i) => i !== index
+          );
+        } else {
+          this.checkedSequences.push(index);
+        }
       }
-      this.isEnriching = false;
-      let msg =
-        selectedWords.length +
-        " séquences ont été enrichies avec succès en néérlandais.";
-      console.log(msg);
-      new Notification(msg);
     },
+
+    toggleCheckedSequence(index) {
+      const isChecked = this.checkedSequences.includes(index);
+      if (isChecked) {
+        this.checkedSequences = this.checkedSequences.filter(
+          (i) => i !== index
+        );
+      } else {
+        this.checkedSequences.push(index);
+      }
+    },
+    selectAll() {
+      if (this.checkedSequences.length === this.sequences.length) {
+        this.checkedSequences = [];
+      } else {
+        this.checkedSequences = this.sequences.map((_, index) => index);
+      }
+    },
+
     async enrichInEnglish() {
-      const selectedWords = this.words.filter((word, index) => {
-        return this.checkedWords[index];
-      });
+      this.enrichProgression = 0;
+      this.isEnriching = true;
+
+      console.log("checkedSequences.length: " + this.checkedSequences.length);
+
       this.enrichProgression = 0;
       this.isEnriching = true;
       let enrichCount = 0;
-      let total = selectedWords.length;
-      for (const sequence of selectedWords) {
-        let id = sequence.id;
+      const selectedSequences = [];
+
+      for (let i = 0; i < this.checkedSequences.length; i++) {
+          selectedSequences.push(this.sequences[i]);
+      }
+
+      console.log("selectedSequences.length: " + selectedSequences.length);
+
+      const total = selectedSequences.length;
+      for (const sequence of selectedSequences) {
+        const id = sequence.id;
         await axios.post(
           `https://localhost:${backendPort}/api/v1/sequences/Dictionary/english?id=${id}`
         );
@@ -57,30 +84,64 @@ export default {
         this.enrichProgression = Math.round((enrichCount * 100) / total);
       }
       this.isEnriching = false;
-
-      let msg =
-        selectedWords.length +
+      const msg =
+        selectedSequences.length +
         " séquences ont été enrichies avec succès en anglais.";
       console.log(msg);
       new Notification(msg);
     },
+
+    async enrichInDutch() {
+      this.enrichProgression = 0;
+      this.isEnriching = true;
+
+      console.log("checkedSequences.length: " + this.checkedSequences.length);
+
+      this.enrichProgression = 0;
+      this.isEnriching = true;
+      let enrichCount = 0;
+      const selectedSequences = [];
+
+      for (let i = 0; i < this.checkedSequences.length; i++) {
+          selectedSequences.push(this.sequences[i]);
+      }
+
+      console.log("selectedSequences.length: " + selectedSequences.length);
+
+      const total = selectedSequences.length;
+      for (const sequence of selectedSequences) {
+        const id = sequence.id;
+        await axios.post(
+          `https://localhost:${backendPort}/api/v1/sequences/Dictionary/dutch?id=${id}`
+        );
+        enrichCount++;
+        this.enrichProgression = Math.round((enrichCount * 100) / total);
+      }
+      this.isEnriching = false;
+      const msg =
+        selectedSequences.length +
+        " séquences ont été enrichies avec succès en néerlandais.";
+      console.log(msg);
+      new Notification(msg);
+    },
+
     async sendToAnki() {
-      const selectedWords = this.words.filter((word, index) => {
-        return this.checkedWords[index];
-      });
+      
+      const selectedSequences = [];
+
+      for (let i = 0; i < this.checkedSequences.length; i++) {
+          selectedSequences.push(this.sequences[i]);
+      }
+
       this.sendToAnkiProgression = 0;
       let sendToAnkiCount = 0;
-      let total = selectedWords.length;
-      this.isSendingToAnki = true;
-      for (const sequence of selectedWords) {
-        let id = sequence.id;
+      const total = selectedSequences.length;
+      for (const sequence of selectedSequences) {
+        const id = sequence.id;
         await axios
           .post(
             `https://localhost:${backendPort}/api/v1/sequences/send-to-anki?id=${id}`
           )
-          .then(() => {
-            new Notification(`${sequence.word} successfully sent to Anki.`);
-          })
           .catch(() => {
             new Notification(`${sequence.word} failed to be sent to Anki.`);
           });
@@ -89,8 +150,9 @@ export default {
           (sendToAnkiCount * 100) / total
         );
       }
-      this.isSendingToAnki = false;
+      new Notification(`${selectedSequences.length} sequences sent to Anki.`);
     },
+
     openFilePicker() {
       this.filePickerDialog = true;
     },
@@ -102,15 +164,6 @@ export default {
     },
     onJsonSelected(event) {
       this.selectedJson = event.target.files[0];
-    },
-    selectAll() {
-      if (this.checkedWords.some((isChecked) => isChecked)) {
-        // Au moins un élément est sélectionné, donc on les désélectionne tous
-        this.checkedWords = [];
-      } else {
-        // Aucun élément n'est sélectionné, donc on les sélectionne tous
-        this.checkedWords = this.words.map(() => true);
-      }
     },
     async importZip() {
       try {
@@ -128,11 +181,11 @@ export default {
             }
           )
           .then((response) => {
-            this.words = response.data;
+            this.sequences = response.data;
             console.log(
-              this.words.length + " words set into the variable 'words'."
+              this.sequences.length + " words set into the variable 'words'."
             );
-            new Notification(this.words.length + " mots ont été importés.");
+            new Notification(this.sequences.length + " mots ont été importés.");
           });
       } catch (error) {
         console.error(error);
@@ -159,14 +212,12 @@ export default {
             }
           )
           .then((response) => {
-            this.words = response.data;
+            this.sequences = response.data;
+
+            let msg = "Importation du fichier Json avec succès.";
+            console.log(msg);
+            new Notification(msg);
           });
-        console.log(
-          "import-details http call ended with status: " + response.status
-        );
-        let msg = "Importation du fichier Json avec succès.";
-        console.log(msg);
-        new Notification(msg);
       } catch (error) {
         console.error(error);
 
@@ -183,14 +234,21 @@ export default {
 </script>
 <template>
   <div class="container mt-5">
-
     <!-- Importer un fichier Zip -->
     <div class="card mb-4">
       <div class="card-header">Sélectionner un fichier Zip</div>
       <div class="card-body">
         <div class="input-group">
-          <input type="file" class="form-control" ref="fileInput" accept=".zip" @change="onFileSelected" />
-          <button class="btn btn-primary ml-3" @click="importZip">Importer</button>
+          <input
+            type="file"
+            class="form-control"
+            ref="fileInput"
+            accept=".zip"
+            @change="onFileSelected"
+          />
+          <button class="btn btn-primary ml-3" @click="importZip">
+            Importer
+          </button>
         </div>
       </div>
     </div>
@@ -200,8 +258,16 @@ export default {
       <div class="card-header">Sélectionner un fichier JSON</div>
       <div class="card-body">
         <div class="input-group">
-          <input type="file" class="form-control" ref="fileInput" accept=".json" @change="onJsonSelected" />
-          <button class="btn btn-primary ml-3" @click="importJSON">Importer</button>
+          <input
+            type="file"
+            class="form-control"
+            ref="fileInput"
+            accept=".json"
+            @change="onJsonSelected"
+          />
+          <button class="btn btn-primary ml-3" @click="importJSON">
+            Importer
+          </button>
         </div>
       </div>
     </div>
@@ -210,14 +276,24 @@ export default {
     <div class="card mb-4">
       <div class="card-header">
         <div class="d-flex align-items-center">
-          <div v-if="isEnriching" class="spinner-border text-primary mr-2" role="status"></div>
+          <div
+            v-if="isEnriching"
+            class="spinner-border text-primary mr-2"
+            role="status"
+          ></div>
           Enrichir {{ this.enrichProgression }}%
         </div>
       </div>
       <div class="card-body">
-        <button class="btn btn-secondary mr-2" @click="selectAll()">Sélectionner tout</button>
-        <button class="btn btn-info mr-2" @click="enrichInEnglish()">Enrichir en anglais</button>
-        <button class="btn btn-info" @click="enrichInDutch()">Enrichir en néérlandais</button>
+        <button class="btn btn-secondary mr-2" @click="selectAll()">
+          Sélectionner tout
+        </button>
+        <button class="btn btn-info mr-2" @click="enrichInEnglish()">
+          Enrichir en anglais
+        </button>
+        <button class="btn btn-info" @click="enrichInDutch()">
+          Enrichir en néérlandais
+        </button>
       </div>
     </div>
 
@@ -225,37 +301,47 @@ export default {
     <div class="card mb-4">
       <div class="card-header">
         <div class="d-flex align-items-center">
-          <div v-if="isSendingToAnki" class="spinner-border text-primary mr-2" role="status"></div>
+          <div
+            v-if="isSendingToAnki"
+            class="spinner-border text-primary mr-2"
+            role="status"
+          ></div>
           Envoyer {{ this.sendToAnkiProgression }}%
         </div>
       </div>
       <div class="card-body">
-        <button class="btn btn-primary" @click="sendToAnki()">Envoyer vers Anki</button>
+        <button class="btn btn-primary" @click="sendToAnki()">
+          Envoyer vers Anki
+        </button>
       </div>
     </div>
 
-    <!-- Tableau des mots -->
+    <!-- Liste des mots -->
     <div class="card">
       <div class="card-body">
-        <table class="table">
-          <tbody>
-            <tr v-for="(file, index) in words" :key="file.name">
-              <td>
-                <input type="checkbox" class="form-check-input" v-model="checkedWords[index]" />
-                <span>{{ file.word }}</span>
-              </td>
-              <td>
-                <span>{{ file.translatedWord }}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="list-group">
+          <div
+            v-for="(sequence, index) in sequences"
+            :key="sequence.id"
+            :class="[
+              'list-group-item',
+              { active: checkedSequences.includes(index) },
+            ]"
+            @mousedown="selectSequence(index, $event.shiftKey)"
+            @keydown.space.prevent="toggleCheckedSequence(index)"
+            tabindex="0"
+          >
+            <input
+              type="checkbox"
+              class="form-check-input"
+              v-model="checkedSequences"
+              :value="index"
+            />
+            <span>{{ sequence.word }}</span>
+            <span class="ml-2">{{ sequence.translatedWord }}</span>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
-
-<style scoped>
-/* Pas besoin de styles spécifiques ici car nous utilisons Bootstrap */
-</style>
