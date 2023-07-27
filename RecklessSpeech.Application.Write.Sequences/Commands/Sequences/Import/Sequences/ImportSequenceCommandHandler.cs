1a65e879fd38aa8b631d0e72fc3ebcab6f3b61f0
Felix <55741682+RogerHanain@users.noreply.github.com>
2023-07-27 23:57:50 +0200

    replace html with placeholder

---
+++ RecklessSpeech.Application.Write.Sequences/Commands/Sequences/Import/Sequences/ImportSequenceCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using RecklessSpeech.Application.Write.Sequences.Ports;
+using RecklessSpeech.Domain.Sequences.Sequences;
+
+namespace RecklessSpeech.Application.Write.Sequences.Commands.Sequences.Import.Sequences
+{
+    public class ImportSequenceCommandHandler : IRequestHandler<ImportSequenceCommand>
+    {
+        private readonly ISequenceRepository sequenceRepository;
+        public ImportSequenceCommandHandler(ISequenceRepository sequenceRepository)
+        {
+            this.sequenceRepository = sequenceRepository;
+
+        }
+        public Task<Unit> Handle(ImportSequenceCommand request, CancellationToken cancellationToken)
+        {
+            Word word = Word.Create(request.Word);
+            TranslatedSentence translatedSentence = TranslatedSentence.Create(request.TranslatedSentence);
+            AudioFileNameWithExtension audio = AudioFileNameWithExtension.Create(request.AudioFilenameWithExtension);
+            MediaId mediaId = MediaId.Create(request.MediaId);
+
+            HtmlContent htmlContent = default;
+            //= GetHtmlContent(mediaId, translatedSentence);
+
+            Sequence sequence = Sequence.Create(Guid.NewGuid(),
+                htmlContent,
+                audio,
+                word,
+                translatedSentence,
+                mediaId);
+
+            this.sequenceRepository.Add(sequence);
+            
+            return Task.FromResult(Unit.Value);
+        }
+    }
+}
