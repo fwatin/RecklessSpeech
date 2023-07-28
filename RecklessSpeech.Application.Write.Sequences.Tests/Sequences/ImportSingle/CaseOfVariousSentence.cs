d32b876559a65838103859c1d5183eafa60431e6
Felix <55741682+RogerHanain@users.noreply.github.com>
2023-07-28 10:48:42 +0200

    add test for special characters normally fails

--- RecklessSpeech.Application.Write.Sequences.Tests/Sequences/ImportSingle/CaseOfVariousSentence.cs
+++ RecklessSpeech.Application.Write.Sequences.Tests/Sequences/ImportSingle/CaseOfVariousSentence.cs
@@ -28,11 +28,38 @@ namespace RecklessSpeech.Application.Write.Sequences.Tests.Sequences.ImportSingl
 
             //Assert
             string html = this.repository.All.Single().HtmlContent.Value;
-            
-            const string expected = "<span class=\"dc-gap\"><span class=\"dc-down dc-lang-en dc-orig\"" +
-                                    $" style=\"background-color: rgb(157, 0, 0);\">target</span></span>";
-            
-            html.Should().Contain(expected);
+            html.Should().Contain(GetUnderlined("target"));
+        }
+        
+        [Fact]
+        public async Task Case_of_sentence_with_special_characters()
+        {
+            //Arrange
+            ImportSequenceCommand command = new(
+                "target",
+                "this is the target.",
+                "mp3.mp3",
+                4438);
+
+            //Act
+            await this.sut.Handle(command, CancellationToken.None);
+
+            //Assert
+            string html = this.repository.All.Single().HtmlContent.Value;
+            html.Should().Contain(GetNormal("this is the "));
+            html.Should().Contain(GetUnderlined("target"));
+            html.Should().Contain(GetNormal("."));
+        }
+
+        private static string GetUnderlined(string word)
+        {
+            return  $"<span class=\"dc-gap\"><span class=\"dc-down dc-lang-en dc-orig\"" +
+                                    $" style=\"background-color: rgb(157, 0, 0);\">{word}</span></span>";
+        }
+        
+        private static string GetNormal(string word)
+        {
+            return $"<span class=\"dc-down dc-lang-en dc-orig\">{word}</span>";
         }
     }
 }
