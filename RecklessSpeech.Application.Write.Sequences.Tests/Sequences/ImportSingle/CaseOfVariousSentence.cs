3e25413982ab6fb2e4323a4244a4c751721da3dd
Felix <55741682+RogerHanain@users.noreply.github.com>
2023-07-28 10:40:01 +0200

    add test

---
+++ RecklessSpeech.Application.Write.Sequences.Tests/Sequences/ImportSingle/CaseOfVariousSentence.cs
@@ -0,0 +1,38 @@
+﻿using RecklessSpeech.Application.Write.Sequences.Commands.Sequences.Import.Sequences;
+
+namespace RecklessSpeech.Application.Write.Sequences.Tests.Sequences.ImportSingle
+{
+    public class CaseOfVariousSentence
+    {
+        private readonly InMemorySequenceRepository repository;
+        private ImportSequenceCommandHandler sut;
+
+        public CaseOfVariousSentence()
+        {
+            this.repository = new InMemorySequenceRepository();
+            this.sut = new(this.repository);
+        }
+
+        [Fact]
+        public async Task Case_of_sentence_has_only_one_word()
+        {
+            //Arrange
+            ImportSequenceCommand command = new(
+                "target",
+                "target",
+                "mp3.mp3",
+                4438);
+
+            //Act
+            await this.sut.Handle(command, CancellationToken.None);
+
+            //Assert
+            string html = this.repository.All.Single().HtmlContent.Value;
+            
+            const string expected = "<span class=\"dc-gap\"><span class=\"dc-down dc-lang-en dc-orig\"" +
+                                    $" style=\"background-color: rgb(157, 0, 0);\">target</span></span>";
+            
+            html.Should().Contain(expected);
+        }
+    }
+}
