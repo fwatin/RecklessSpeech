dd7dde22111d9a89c736f466b5324682095a07fa
Felix Watine <felix.watine@masteos.com>
2023-09-22 10:49:51 +0200

    start add

--- RecklessSpeech.Domain.Sequences/Explanations/Language.cs
+++ RecklessSpeech.Domain.Sequences/Explanations/Language.cs
@@ -9,6 +9,11 @@ namespace RecklessSpeech.Domain.Sequences.Explanations
     {
         public override string GetLanguageInFrench() => "anglais";
     }
+    
+    public class Italian : Language
+    {
+        public override string GetLanguageInFrench() => "italien";
+    }
 
     public class Dutch : Language
     {
