0a0961ec4a3b98036afd4edeaac59cd926ad01f8
Felix Watine <felix.watine@masteos.com>
2024-05-09 11:43:10 +0200

    add test with single phrase

---
+++ RecklessSpeech.Web.AcceptanceTests/SequenceControllerTests.cs
@@ -0,0 +1,26 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using RecklessSpeech.Web.Controllers;
+
+namespace RecklessSpeech.Web.AcceptanceTests;
+
+public class SequenceControllerTests
+{
+    [Fact]
+    public async Task ImportSinglePhrase()
+    {
+        //Arrange
+        IMediator dispatcher = Substitute.For<IMediator>();
+        SequenceController controller = new(dispatcher);
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "single-phrase.json");
+        IFormFile formFile;
+        await using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            formFile = new FormFile(stream, 0, stream.Length, "file", Path.GetFileName(filePath));
+        }
+
+        //Act
+        var result = await controller.ImportJson(formFile);
+    }
+}
--- RecklessSpeech.Web.AcceptanceTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace RecklessSpeech.Web.AcceptanceTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
